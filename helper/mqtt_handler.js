58d72a4ba2e8fd66f5ad8e0cdcf25bd3e13a1efb
Lasse Sode <201708610@post.au.dk>
2021-11-05 13:25:24 +0100

    actually added mqtt handler

---
+++ helper/mqtt_handler.js
@@ -0,0 +1,90 @@
+const mqtt = require('mqtt')
+
+const host = 'broker.emqx.io'
+const port = '1883'
+const clientId = `cloudMqtt`
+
+const connectUrl = `http://${host}:${port}`
+// `mqtt://${host}:${port}`
+const client = mqtt.connect(connectUrl, {
+    clientId,
+    clean: true,
+    connectTimeout: 4000,
+    reconnectPeriod: 1000,
+})
+
+console.log(connectUrl)
+// const topic = 'armUp'
+
+client.on('connect', () => {
+    console.log('Connected')
+    // client.subscribe([topic], () => {
+    //   console.log(`Subscribe to topic '${topic}'`)
+    //   console.log('msg', clientId)
+    // })
+    // publish()
+
+});
+
+
+function publishArmUp() {
+
+    topic = "armUp"
+    client.publish(topic, "message from cloud - armUp", { qos: 0, retain: false }, (error) => {
+        if (error) {
+            console.error(error)
+        }
+    }, (complete) => {
+        client.end()
+    })
+}
+
+function publishArmDown() {
+    topic = "armDown"
+    client.publish(topic, "message from cloud - armDown", { qos: 0, retain: false }, (error) => {
+        if (error) {
+            console.error(error)
+        }
+    })
+}
+
+function publishSad() {
+    topic = "sad"
+    client.publish(topic, "message from cloud - sadface", { qos: 0, retain: false }, (error) => {
+        if (error) {
+            console.error(error)
+        }
+    })
+}
+
+function publishHappy() {
+    topic = "happy"
+    client.publish(topic, "message from cloud - happyface", { qos: 0, retain: false }, (error) => {
+        if (error) {
+            console.error(error)
+        }
+    })
+}
+
+module.exports = {
+    publishArmUp: function () {
+        publishArmUp();
+    },
+
+    publishArmDown: function () {
+        publishArmDown();
+    },
+
+    publishSad: function () {
+        publishSad();
+    },
+
+    publishHappy: function () {
+        publishHappy();
+    },
+
+}
+
+
+
+
