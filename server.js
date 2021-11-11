4e854fdf13fc0a9d83d5af5fc3162677433c5da1
Lasse Sode <201708610@post.au.dk>
2021-11-11 17:43:47 +0100

    reverted back to http

--- server.js
+++ server.js
@@ -4,18 +4,18 @@ let port = process.env.PORT;
 var https = require('https');
 var fs = require('fs');
 //regular http server
-// var server = http.createServer(app);
-// server.listen(port, function() {
-//   console.log(`${port} is opened`);
-// });
-
-//https server code
-var options = {
-  key: fs.readFileSync('../interactionbot/client-key.pem'),
-  cert: fs.readFileSync('../interactionbot/client-cert.pem')
-};
-
-var server = https.createServer(options, app);
+var server = http.createServer(app);
 server.listen(port, function() {
   console.log(`${port} is opened`);
 });
+
+// // //https server code
+// var options = {
+//   key: fs.readFileSync('../interactionbot/client-key.pem'),
+//   cert: fs.readFileSync('../interactionbot/client-cert.pem')
+// };
+
+// var server = https.createServer(options, app);
+// server.listen(port, function() {
+//   console.log(`${port} is opened`);
+// });
